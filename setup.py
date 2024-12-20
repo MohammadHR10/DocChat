8515b0667505206207994ec3b004f667a17ab4a4
MohammadHR10 <mhr9808@mavs.uta.edu>
2024-12-19 22:55:00 -0600

    initial folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'DocChat',
+    version= '0.0.0',
+    author= 'Mohammad',
+    author_email= 'dipto.rh007@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
